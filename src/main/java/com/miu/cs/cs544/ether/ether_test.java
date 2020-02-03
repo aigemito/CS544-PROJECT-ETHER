ced942967bc19e043f155c3ee4775375fb3b923e
Amanuel Isack Gebreendrias <38761168+aigemito@users.noreply.github.com>
2020-02-03 00:51:03 -0600

    First-time-committed

---
+++ src/main/java/com/miu/cs/cs544/ether/ether_test.java
@@ -0,0 +1,14 @@
+package com.miu.cs.cs544.ether;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ether_test {
+	@GetMapping("/ether/{membername}")
+	public String getEtherMessage(@PathVariable String membername) {
+		return "Hello,"+membername;
+	}
+}
